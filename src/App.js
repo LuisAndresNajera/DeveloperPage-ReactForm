c428c966b5ac2e78d476d5390501ee295f126953
Luis Andres Najera Couoh <andresnajera01@gmail.com>
2024-05-28 20:51:21 -0600

    adding-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Asegúrate de tener este archivo si lo necesitas
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className="titulo">Luis Najera <span>Developer</span></h1>
       </header>
+      <form className="formulario">
+        <fieldset>
+          <legend>Contactanos llenando todos los campos</legend>
+          <div className="contenedor-campos">
+            <div className="campo">
+              <label>Nombre</label>
+              <input className="input-text" type="text" placeholder="Tu Nombre" required />
+            </div>
+            <div className="campo">
+              <label>Telefono</label>
+              <input className="input-text" type="tel" placeholder="Tu número" required />
+            </div>
+            <div className="campo">
+              <label>Correo</label>
+              <input className="input-text" type="email" placeholder="Dirección de correo electrónico" required />
+            </div>
+            <div className="campo">
+              <label>Mensaje</label>
+              <textarea className="input-text" name="" id="" cols="30" rows="10" required></textarea>
+            </div>
+          </div>
+          <div className="alinear-derecha flex">
+            <input className="boton w-sm-100" type="submit" value="Enviar" />
+          </div>
+        </fieldset>
+      </form>
     </div>
   );
 }
